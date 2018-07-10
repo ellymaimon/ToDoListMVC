545ef506b54f31722417e56088d936c7b587f216
Elly Maimon <eliranmaimon@gmail.com>
2018-07-09 21:36:21 -0700

    database lesson

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -71,5 +71,19 @@ namespace ToDoList.Tests
             Assert.AreEqual(testId, result);
         }
 
+        [TestMethod]
+        public void Find_FindsItemInDatabase_Item()
+        {
+            //Arrange
+            Item testItem = new Item("Mow the lawn");
+            testItem.Save();
+
+            //Act
+            Item foundItem = Item.Find(testItem.GetId());
+
+            //Assert
+            Assert.AreEqual(testItem, foundItem);
+        }
+
     }
 }
