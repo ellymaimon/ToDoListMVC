69eec650a9ddfcffda7bab72c39be1775be15dc2
Elly Maimon <eliranmaimon@gmail.com>
2018-07-15 22:13:10 -0700

    join_table next lesson

--- ToDoList.Tests/ModelTests/CategoryTest.cs
+++ ToDoList.Tests/ModelTests/CategoryTest.cs
@@ -88,5 +88,73 @@ namespace ToDoList.Tests
             Item.DeleteAll();
             Category.DeleteAll();
         }
+
+        [TestMethod]
+        public void Delete_DeletesCategoryAssociationsFromDatabase_CategoryList()
+        {
+            //Arrange
+            Item testItem = new Item("Mow the lawn");
+            testItem.Save();
+
+            string testName = "Home stuff";
+            Category testCategory = new Category(testName);
+            testCategory.Save();
+
+            //Act
+            testCategory.AddItem(testItem);
+            testCategory.Delete();
+
+            List<Category> resultItemCategories = testItem.GetCategories();
+            List<Category> testItemCategories = new List<Category> { };
+
+            //Assert
+            CollectionAssert.AreEqual(testItemCategories, resultItemCategories);
+        }
+
+        [TestMethod]
+        public void Test_AddItem_AddsItemToCategory()
+        {
+            //Arrange
+            Category testCategory = new Category("Household chores");
+            testCategory.Save();
+
+            Item testItem = new Item("Mow the lawn");
+            testItem.Save();
+
+            Item testItem2 = new Item("Water the garden");
+            testItem2.Save();
+
+            //Act
+            testCategory.AddItem(testItem);
+            testCategory.AddItem(testItem2);
+
+            List<Item> result = testCategory.GetItems();
+            List<Item> testList = new List<Item> { testItem, testItem2 };
+
+            //Assert
+            CollectionAssert.AreEqual(testList, result);
+        }
+
+        [TestMethod]
+        public void GetItems_ReturnsAllCategoryItems_ItemList()
+        {
+            //Arrange
+            Category testCategory = new Category("Household chores");
+            testCategory.Save();
+
+            Item testItem1 = new Item("Mow the lawn");
+            testItem1.Save();
+
+            Item testItem2 = new Item("Buy plane ticket");
+            testItem2.Save();
+
+            //Act
+            testCategory.AddItem(testItem1);
+            List<Item> savedItems = testCategory.GetItems();
+            List<Item> testList = new List<Item> { testItem1 };
+
+            //Assert
+            CollectionAssert.AreEqual(testList, savedItems);
+        }
     }
 }
